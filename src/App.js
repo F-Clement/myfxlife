7dc7c434ed28e0e1579da613fb40ae943d62b726
F-Clement <fonyuyclement@gmail.com>
2024-02-12 13:37:09 +0100

    Add Route to our app and some styles to the navbar elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Container } from 'react-bootstrap';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Home Page</h1>} />
+          <Route exact path="/posts" render={() => <h1>Posts</h1>} />
+          <Route exact path="/login" render={() => <h1>Login</h1>} />
+          <Route exact path="/logout" render={() => <h1>Logout</h1>} />
+          <Route exact path="/signup" render={() => <h1>Signup</h1>} />
+          <Route exact path="/profile" render={() => <h1>Profile</h1>} />
+          <Route render={() => <p>Sorry! Page Not Found.</p>} />
+        </Switch>
 
       </Container>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,37 @@ import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import logo from '../assets/logo.png'
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom/cjs/react-router-dom.min'
 
 const NavBar = () => {
   return (
-    <Navbar className='styles.NavBar' bg="light" expand="md" fixed='top'>
+    <Navbar className={styles.NavBar} bg="light" expand="md" fixed='top'>
         <Container>
+            <NavLink to="/">
             <Navbar.Brand>
                 <img src={logo} alt='logo' height='45'/>
-            </Navbar.Brand>
+            </Navbar.Brand></NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <Nav.Link><i className='fas fa-home'></i>Home</Nav.Link>
-                    <Nav.Link><i class="fa-solid fa-folder-open"></i>Posts</Nav.Link>
-                    <Nav.Link><i className='fas fa-sign-in-alt'></i>Login</Nav.Link>
-                    <Nav.Link><i class="fa-solid fa-right-from-bracket"></i>Logout</Nav.Link>
-                    <Nav.Link><i className='fas fa-user-plus'></i>Signup</Nav.Link>
-                    <Nav.Link><i class="fa-regular fa-id-card"></i>Profile</Nav.Link>
+                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+                        <i className='fas fa-home'></i>Home
+                    </NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="posts">
+                        <i class="fa-solid fa-folder-open"></i>Posts
+                    </NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="login">
+                        <i className='fas fa-sign-in-alt'></i>Login
+                    </NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="logout">
+                        <i class="fa-solid fa-right-from-bracket"></i>Logout
+                    </NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="signup">
+                        <i className='fas fa-user-plus'></i>Signup
+                    </NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="profile">
+                        <i class="fa-regular fa-id-card"></i>Profile
+                    </NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Container>

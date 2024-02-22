57914dd134a983141d3ca5a9aa3e20af13b801ad
Clement Fonyuy <fonyuyclement@gmail.com>
2024-02-22 12:53:18 +0000

    Complete post creation and enable buttons to cancel to return to prev page or submit to posts page

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
 
 import Upload from "../../assets/upload.png";
 
@@ -13,6 +14,8 @@ import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
 import btnStyles from "../../styles/Button.module.css";
 import { Image } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function PostCreateForm() {
 
@@ -24,6 +27,9 @@ function PostCreateForm() {
     })
 
     const { title, content, image } = postData;
+
+    const imageInput = useRef(null)
+    const history = useHistory()
 
     const handleChange = (event) => {
         setPostData({
@@ -42,6 +48,25 @@ function PostCreateForm() {
         }
     };
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+
+        formData.append("title", title);
+        formData.append("content", content);
+        formData.append("image", imageInput.current.files[0]);
+
+        try {
+            const { data } = await axiosReq.post("/posts/", formData);
+            history.push(`/posts/${data.id}`);
+          } catch (err) {
+            console.log(err);
+            if (err.response?.status !== 401) {
+              setErrors(err.response?.data);
+            }
+        }
+    }
+
     const textFields = (
         <div className="text-center">
             <Form.Group>
@@ -53,6 +78,11 @@ function PostCreateForm() {
                     onChange={handleChange}
                 />
             </Form.Group>
+            {errors?.title?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
             <Form.Group>
                 <Form.Label>Content</Form.Label>
                 <Form.Control
@@ -63,10 +93,15 @@ function PostCreateForm() {
                     onChange={handleChange}
                 />
             </Form.Group>
+            {errors?.content?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
 
             <Button
                 className={`${btnStyles.Button} ${btnStyles.Blue}`}
-                onClick={() => { }}
+                onClick={() => {history.goBack() }}
             >
                 cancel
             </Button>
@@ -77,7 +112,7 @@ function PostCreateForm() {
     );
 
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Row>
                 <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
                     <Container
@@ -87,7 +122,7 @@ function PostCreateForm() {
                             {image ? (
                                 <>
                                     <figure>
-                                        <Image className="appStyles.Image" src={image} rounded />
+                                        <Image className="col-md-12" src={image} rounded />
                                     </figure>
                                     <div>
                                         <Form.Label
@@ -104,12 +139,17 @@ function PostCreateForm() {
                                     className="d-flex justify-content-center"
                                     htmlFor="image-upload"
                                 >
-                                    <Asset src={Upload} message="Click to upload image" />
+                                    <Asset src={Upload} message="Upload Image" />
                                 </Form.Label>
 
                             )}
 
-                            <Form.File id="image-upload" accept="image/*" onChange={handleChangeImage} />
+                            <Form.File
+                                id="image-upload"
+                                accept="image/*"
+                                onChange={handleChangeImage}
+                                ref={imageInput} 
+                            />
 
                         </Form.Group>
                         <div className="d-md-none">{textFields}</div>

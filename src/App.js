5a40eeb78f8043de6b449d34e802866a5fc13f71
Clement Fonyuy <fonyuyclement@gmail.com>
2024-02-24 15:11:50 +0000

    Add posts to post page and a search bar to search posts

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import SignUpForm from './pages/auth/SignUpForm';
 import './api/axiosDefaults';
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
-import PostPage from './pages/posts/PostPage';
+// import PostPage from './pages/posts/PostPage';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import PostsPage from './pages/posts/PostsPage';
+import { useCurrentUser } from './contexts/CurrentUserContext';
 
 
 function App() {
+
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
 
   return (
         <div className={styles.App}>
@@ -26,7 +30,7 @@ function App() {
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/profile" render={() => <h1>Profile</h1>} />
               <Route exact path="/create/post" render={() => <PostCreateForm /> } />
-              <Route exact path="/posts/:id" render={() => <PostPage /> } />
+              <Route exact path="/posts/:id" render={() => <PostsPage message = "No results for this search" /> } />
               <Route render={() => <p>Sorry! Page Not Found.</p>} />
             </Switch>
           </Container>
--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,25 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-// import Form from "react-bootstrap/Form";
+import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-// import Container from "react-bootstrap/Container";
+import Container from "react-bootstrap/Container";
+import Post from "./Post";
+import Asset from "../../components/Asset";
+import NoResults from "../../assets/no-results.png";
 
-// import appStyles from "../../App.module.css";
-// import styles from "../../styles/PostsPage.module.css";
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
 
-function PostsPage() {
-  
+// import { useLocation } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+
+
+function PostsPage({ message, filter = "" }) {
+
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const {pathname} = useLocation();
+
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, query, pathname]);
+
+
   return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
-      </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
-      </Col>
-    </Row>
+    <div>
+
+      <Container>
+        <Row>
+          <Col sm>sm=true</Col>
+          <Col sm>
+            <Row className="h-100">
+              <Col className="py-2 p-0 p-lg-2" lg={8}>
+                {/* <p>Popular profiles mobile</p> */}
+                {hasLoaded ? (
+                  <>
+                    {posts.results.length ? (
+                      posts.results.map((post) => (
+                        <Post key={post.id} {...post} setPosts={setPosts} />
+                      ))
+                    ) : (
+                      <Container className={appStyles.Content}>
+                        <Asset src={NoResults} message={message} />
+                      </Container>
+                    )}
+                  </>
+                ) : (
+                  <Container className={appStyles.Content}>
+                    <Asset spinner />
+                  </Container>
+                )}
+              </Col>
+              {/* <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+                <p>Popular profiles for desktop</p>
+              </Col> */}
+            </Row>
+          </Col>
+          <Col sm>
+          <i className={`fas fa-search ${styles.SearchIcon}`} />
+          <Form
+            className={styles.SearchBar}
+              onSubmit={(event) => event.preventDefault()}
+            >
+              <Form.Control
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
+                type="text"
+                className="mr-sm-2"
+                placeholder="Search posts"
+              />
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+
+    </div>
   );
 }
 

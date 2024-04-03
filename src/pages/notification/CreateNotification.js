6a593c0fbb5f0d29ad67defbe0ca703f2a73ae64
Clement Fonyuy <fonyuyclement@gmail.com>
2024-04-03 14:17:50 +0000

    Finishing touches

--- src/pages/notification/CreateNotification.js
+++ src/pages/notification/CreateNotification.js
@@ -15,104 +15,103 @@ import btnStyles from "../../styles/Button.module.css";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-
 function CreateNotification() {
-    const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({});
 
-    const [notificationData, setNotificationData] = useState({
-        title: "",
-        content: ""
+  const [notificationData, setNotificationData] = useState({
+    title: "",
+    content: "",
+  });
+  const { title, content } = notificationData;
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setNotificationData({
+      ...notificationData,
+      [event.target.name]: event.target.value,
     });
-    const { title, content } = notificationData;
-    const history = useHistory();
+  };
 
-    const handleChange = (event) => {
-        setNotificationData({
-            ...notificationData,
-            [event.target.name]: event.target.value,
-        });
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const formData = new FormData();
+    formData.append("title", title);
+    formData.append("content", content);
 
-        formData.append("title", title);
-        formData.append("content", content);
-
-        try {
-            const {data} = await axiosReq.post("/notifications/", formData);
-            history.push(`/info/${data.id}`);
-        } catch (err) {
-            console.log(err.response.data)
-            if (err.response?.status !== 401) {
-                setErrors(err.response?.data);
-                console.log(err)
-            }
-        }
+    try {
+      const { data } = await axiosReq.post("/notifications/", formData);
+      history.push(`/info/${data.id}`);
+    } catch (err) {
+      console.log(err.response.data);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+        console.log(err);
+      }
     }
+  };
 
-    const textFields = (
-        <div className="text-center">
-            <Form.Group>
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                    type="text"
-                    name="title"
-                    value={title}
-                    onChange={handleChange}
-                />
-            </Form.Group>
-            {errors?.title?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                    {message}
-                </Alert>
-            ))}
-            <Form.Group>
-                <Form.Label>Content</Form.Label>
-                <Form.Control
-                    as="textarea"
-                    rows="6"
-                    name="content"
-                    value={content}
-                    onChange={handleChange}
-                />
-            </Form.Group>
-            {errors?.content?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                    {message}
-                </Alert>
-            ))}
+  const textFields = (
+    <div className="text-center">
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.title?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>Content</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows="6"
+          name="content"
+          value={content}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
 
-            <Button
-                className={`${btnStyles.Button} ${btnStyles.Blue}`}
-                onClick={() => history.goBack()}
-            >
-                cancel
-            </Button>
-            <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-                create
-            </Button>
-        </div>
-    );
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Blue}`}
+        onClick={() => history.goBack()}
+      >
+        cancel
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+        create
+      </Button>
+    </div>
+  );
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Row>
-                <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
-                <p>Send Notification</p>
-                    <Container
-                        className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
-                    >
-                        <Container className={appStyles.Content}>{textFields}</Container> 
-                    </Container>
-                </Col>
-                {/* <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <p>Send Notification</p>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Container className={appStyles.Content}>{textFields}</Container>
+          </Container>
+        </Col>
+        {/* <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
                     <Container className={appStyles.Content}>{textFields}</Container>
                 </Col> */}
-            </Row>
-        </Form>
-    );
+      </Row>
+    </Form>
+  );
 }
 
 export default CreateNotification;
